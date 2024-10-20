b5afb99d612a3e795dd9ef49d9180ab63760c5a2
Byron Lester <125940003+byro23@users.noreply.github.com>
2024-10-20 13:40:06 +1100

    deleted cmake debug folder

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/byroj/CLionProjects/Advanced_Algorithms_AT1
-# Build directory: C:/Users/byroj/CLionProjects/Advanced_Algorithms_AT1/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/byroj/CLionProjects/Advanced_Algorithms_AT1/cmake-build-debug/main[1]_include.cmake")
-subdirs("_deps/googletest-build")
